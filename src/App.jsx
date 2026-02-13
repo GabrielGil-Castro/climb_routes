3595d677c210a7c252467cf3609273ea4b56e899
chaves <chavesprogrammer@gmail.com>
2026-02-13 17:16:56 -0300

    Fix: vite config e browserroute

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/NavBar";
 import Home from "./pages/Home";
@@ -7,16 +7,16 @@ import Legacy from "./pages/Legacy";
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Navbar />
 
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Home />} />
         <Route path="/routes" element={<RoutesPage />} />
         <Route path="/legacy" element={<Legacy />} />
         <Route path="*" element={<Home />} />
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
 
